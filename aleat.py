ce986471c9593942887c3e10a747240cc0581514
Alvaro Lopez Garcia <lopeza@alpha11.aulas.gsyc.urjc.es>
2018-02-27 18:39:36 +0100

    Version definitiva

---
+++ aleat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import webapp
+import random
+
+class myWebapp(webapp.webApp):
+
+    def process(self,parsedRequest):
+            num = random.randint(1,999999)
+            return ("200 OK", "<html><body><h1>It works!</h1><p><a href= http://localhost:1234/"+str(num)+
+                        ">Dame otra</a></p></body></html>")
+
+if __name__ == "__main__":
+    myapp = myWebapp("localhost",1234)
+    
